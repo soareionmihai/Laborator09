3b2e742ffe2b6d6f2e0e6e65379d7b63dc30b107
soareionmihai <soare.mihai1992@google.com>
2015-05-05 20:34:41 +0300

    netestat

--- labtasks/ChatServiceAndroidNSD/src/ro/pub/cs/systems/pdsd/lab09/chatservice/networkservicediscoveryoperations/ChatClient.java
+++ labtasks/ChatServiceAndroidNSD/src/ro/pub/cs/systems/pdsd/lab09/chatservice/networkservicediscoveryoperations/ChatClient.java
@@ -12,6 +12,10 @@ import java.util.concurrent.BlockingQueue;
 import ro.pub.cs.systems.pdsd.lab09.chatservice.general.Constants;
 import ro.pub.cs.systems.pdsd.lab09.chatservice.general.Utilities;
 import ro.pub.cs.systems.pdsd.lab09.chatservice.model.Message;
+import ro.pub.cs.systems.pdsd.lab09.chatservice.view.ChatActivity;
+import ro.pub.cs.systems.pdsd.lab09.chatservice.view.ChatConversationFragment;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.Context;
 import android.util.Log;
 
@@ -84,7 +88,26 @@ public class ChatClient {
 					//   - add the message to the conversationHistory
 					//   - if the ChatConversationFragment is visible (query the FragmentManager for the Constants.FRAGMENT_TAG tag)
 					//   append the message to the graphic user interface
-
+					while (!Thread.currentThread().isInterrupted())
+					{
+						String message = messageQueue.take();
+						if(message != null)
+						{
+							printWriter.println(message);
+							printWriter.flush();
+							Message msg = new Message(message, Constants.MESSAGE_TYPE_SENT);
+							conversationHistory.add(msg);
+							if (context != null) {
+								  ChatActivity chatActivity = (ChatActivity)context;
+								  FragmentManager fragmentManager = chatActivity.getFragmentManager();
+								  Fragment fragment = fragmentManager.findFragmentByTag(Constants.FRAGMENT_TAG);
+								  if (fragment instanceof ChatConversationFragment && fragment.isVisible()) {
+								    ChatConversationFragment chatConversationFragment = (ChatConversationFragment)fragment;
+								    chatConversationFragment.appendMessage(msg);
+								  }
+								}
+						}
+					}
 				} catch (Exception exception) {
 					Log.e(Constants.TAG, "An exception has occurred: " + exception.getMessage());
 					if (Constants.DEBUG) {
@@ -121,6 +144,25 @@ public class ChatClient {
 					//   - add the message to the conversationHistory
 					//   - if the ChatConversationFragment is visible (query the FragmentManager for the Constants.FRAGMENT_TAG tag)
 					//   append the message to the graphic user interface
+					while (!Thread.currentThread().isInterrupted())
+					{
+						String message = bufferedReader.readLine();
+						if(message != null)
+						{
+							
+							Message msg = new Message(message, Constants.MESSAGE_TYPE_RECEIVED);
+							conversationHistory.add(msg);
+							if (context != null) {
+								  ChatActivity chatActivity = (ChatActivity)context;
+								  FragmentManager fragmentManager = chatActivity.getFragmentManager();
+								  Fragment fragment = fragmentManager.findFragmentByTag(Constants.FRAGMENT_TAG);
+								  if (fragment instanceof ChatConversationFragment && fragment.isVisible()) {
+								    ChatConversationFragment chatConversationFragment = (ChatConversationFragment)fragment;
+								    chatConversationFragment.appendMessage(msg);
+								  }
+								}
+						}
+					}
 					
 				} catch (Exception exception) {
 					Log.e(Constants.TAG, "An exception has occurred: " + exception.getMessage());
